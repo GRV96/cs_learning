9155ecb897bbba0f109dc517a7e7492e2fb88b4b
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-12-19 17:01:49 -0500

    A function calculates the greatest common divisor.

--- cs_learning/Mathematics.cs
+++ cs_learning/Mathematics.cs
@@ -7,7 +7,7 @@ namespace cs_learning
         public static ulong Factorial(uint number)
         {
             // An overflow occurs if i >= 21.
-            if (number == 0)
+            if(number == 0)
             {
                 return 1;
             }
@@ -15,13 +15,42 @@ namespace cs_learning
             return number * Factorial(number - 1);
         }
 
-        public static void PrintFactorial(uint number)
+        public static uint GreatestCommonDivisor(int number1, int number2)
         {
-            Console.WriteLine("Factorial of {0}: {1}", number, Factorial(number));
+            // This function executes the Euclidean algorithm.
+            number1 = Math.Abs(number1);
+            number2 = Math.Abs(number2);
+
+            while (number2 > 0)
+            {
+                int remainder = number1 % number2;
+                number1 = number2;
+                number2 = remainder;
+            }
+
+            return (uint) number1;
+        }
+
+        private static void PrintFactorial(uint number)
+        {
+            Console.WriteLine("Factorial of {0}: {1}",
+                number, Factorial(number));
+        }
+
+        private static void PrintGcd(int number1, int number2)
+        {
+            Console.WriteLine("GCD({0}, {1}): {2}",
+                number1, number2, GreatestCommonDivisor(number1, number2));
         }
 
         public static void RunFunctions()
         {
+            PrintGcd(7, 0);
+            PrintGcd(19, 7);
+            PrintGcd(720, 96);
+
+            Console.WriteLine();
+
             for(int i=0; i<=20; i++)
             {
                 PrintFactorial((uint) i);
