c6f46e0ea381de90841e432857caf6a6c592191f
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-12-20 12:16:15 -0500

    Function IsLeapYear

--- cs_learning/Miscellanous.cs
+++ cs_learning/Miscellanous.cs
@@ -4,6 +4,16 @@ namespace cs_learning
 {
     class Miscellanous
     {
+        public static bool IsLeapYear(int year)
+        {
+            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        }
+
+        private static void PrintIsLeapYear(int year)
+        {
+            Console.WriteLine("{0} is a leap year: {1}", year, IsLeapYear(year));
+        }
+
         private static void PrintSwappedData(dynamic data1, dynamic data2)
         {
             Console.WriteLine("Data before swap: {0}; {1}", data1, data2);
@@ -19,6 +29,16 @@ namespace cs_learning
             PrintSwappedData(Pi, Phi);
             Console.WriteLine();
             PrintSwappedData(Pi, PiAsStr);
+
+            Console.WriteLine();
+
+            PrintIsLeapYear(2004);
+            PrintIsLeapYear(2000);
+            PrintIsLeapYear(1996);
+            PrintIsLeapYear(1904);
+            PrintIsLeapYear(1900);
+            PrintIsLeapYear(1896);
+            PrintIsLeapYear(1895);
         }
 
         public static void SwapData(ref dynamic data1, ref dynamic data2)
